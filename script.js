bc22c486274f724a976308c0d1c5c68fa6586bb4
Rodrigo <rodrigodevelop1983@outlook.com>
2024-11-15 17:36:25 -0400

    primeiro commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Caso precise de funcionalidades dinâmicas, como carregamento de vídeos, integração com mapas etc.
+document.addEventListener('DOMContentLoaded', function() {
+    // Código JS aqui
+});
